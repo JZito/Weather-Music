292ef4defd5ddfcfc0294edc3a8368dcfead3754
Jeff <oakisland@gmail.com>
2015-05-05 18:25:56 -0400

    refactoring, modules

--- TestNoteAndChord.js
+++ TestNoteAndChord.js
@@ -1,107 +1,20 @@
-var vanillaNotes = [12,11,9,7,4,2,0], beets = [1, 1/2, 1/4, 1/8,1/16, 1/32], beepEM = 77, rotations = [2,4,6,8], measureCount = 0, mainScore;
+var vanillaNotes = [12,11,9,7,4,2,0], vanillaMeasures = [1,2,4,6,8,12,16], beets = [1, 1/2, 1/4, 1/8,1/16, 1/32], beepEM = 77, rotations = [2,4,6,8], measureCount = 0, mainScore;
 
 function setup() {
-	// var strum = Score([0, function(){
-	// 		console.log("struuuummmmmmmmm");
-	// 		p.note.seq(notesArray(0, 4,4), [1/59, 1/64, 1/62]);
-	// 	}, measures(1/16), function(){
- //  			p.note.seq.stop();
-	// 	}, measures(5.9375)]).loop(); 
-
-	// var lovelyGuitar = Score([0, function(){
-	// 		console.log("func one")
-	// 		pp.note.seq(notesArray(12, 2, 12), ReturnBeetsArray(4))
-	// 		ppp.note.seq(notesArray(12,1,2), ReturnBeetsArray(8))
-	// 	}, measures(3), function(){
-	// 		console.log("function two");
-	// 		strum.start()
-	// 	}, measures(1)]).loop();
-
-	// var stopper = Score([0, function(){
-	// 	lovelyGuitar.stop();
-
-	// }, measures(1)]).loop();
-
-canvas = createCanvas( windowWidth, windowHeight );
-Clock.bpm(beepEM);
-drum = XOX('x*x*', 1/16)
-			drum.fx.add(Crush({bitDepth:16}))
-
-//GIT
-p = Synth({preset:'bleepEcho', maxVoices:4})
-pp = Synth({preset:'bleepEcho', maxVoices:4})
-ppp = Synth({preset:'bleepEcho', maxVoices:4})
-ppp.fx.add(Delay({time:1/16.005, feedback:.5, dry: .45, wet:.75}, Reverb('space')))
-//pp.fx.add(Delay({time:.503, feedback:.4, dry: .75, wet:.25}),Reverb({preset:'small', wet:.5, dry:.65}))
-//PAD
-//pad = FM({preset:'radio', maxVoices:4, useADSR:'true', attack:.1, decay:.5, sustain:.25, release:2})
-//pad.fx.add(Tremolo({amp:.55, frequency:(beepEM / 120.1)}), Reverb('space'))
-//pad.chord.seq(LeadMelody.chordsReturn(4,4), 4)
-
-
-
-// a chord series could pick a pedal point
-// for length of chord 1
-// choose pedal point
-// if next number chosen is = pedal point
-// choose a different number 
-// 
-
-
-// score length equals number of chords * random square mult like 2 4 8 16
-// next function
-// pad.chord.seq.stop
-
-
-
-
- var strum = function() {
- 	strumScore = Score([0, function(){
- 			p.note.seq([-12,-8,-5,2], [1/56, 1/64, 1/52]);
- 	}, measures(1/16), function(){
-  			p.note.seq.stop();
-	}, measures(measures[floor(random(measures.length))] + (1-1/16))]).start().loop();
- },
- 	lovelyGuitar = function(){
- 		l = Score([0, function(){
-			pp.note.seq(LeadMelody.notesReturn(0, 2, 12), LeadMelody.beetsReturn(2))
-			pp.chord.seq(LeadMelody.chordsReturn(4,4), LeadMelody.beetsReturn(8))
-			ppp.note.seq(LeadMelody.notesReturn(12,4,8), LeadMelody.beetsReturn(1))
-		}, measures(3)]).start().loop();
- 	}, 
-
- 	stopper = function() {
-	 	s = Score([0, function(){
-	 		console.log("stopper");
-		 	l.stop();
-		 	pp.note.seq.stop();
-		 	pp.chord.seq.stop();
-		 	ppp.note.seq.stop();
-		 }, measures(1)]).start();
- 	},
-
-	parentScore = function() {
-	 	 s = Score([0, lovelyGuitar, measures(7), stopper, measures(1)]).start().loop();
-	 	//p.note.seq([-1,2,9], 1/16)
- 	};
-
- 	score = Score([0, parentScore, measures(654)]).start().loop();
-	follow = Follow( pp )
+	canvas = createCanvas( windowWidth, windowHeight );
+	Clock.bpm(beepEM);
 }
 
 function draw() {
-	background( follow.getValue() * 255 )
+	// background( follow.getValue() * 255 )
 	// if (measureCount >= 8){
 	// 	mainScore.stop()
 	// 	measureCount = 0;
 	// }
 }
+//name = instrumentKind(pre)
 
-var LeadMelody = (function () {
-
-  	var privateMethod = function () {
-    // private
-  	};
+var Harmony = (function () {
 
   	var notesReturn = function (oct, lowRange, highRange) {
   		var scoreNotes = [];
@@ -188,19 +101,145 @@ var LeadMelody = (function () {
 
 })();
 
-// function ReturnBeetsArray(mul) {
-//   // can declare beat multiplier here by passing it in ()
+var Song = (function () {
 
-// }
+	var syn, syns = [], m = 4, scorePhrases = 9,
+	presets = ['cascade', 'bleep', 'bleepEcho', 'rhodes', 'warble'],
+	padPresets = ['pad2','pad4', 'rainTri' ];
+	//it can all happen in here. handle each score, handle each instrument
+	// array of syn objects can live here but be changed by a public method
+	// syns = [];
+	// public method to restart clears objects
+	// syn is the object to be effected... how to assign?
+	// privtely fill details of score
+    // check the time function will be calling module.clear method to start over
+    // use a circle ? for each follow... needs to be referenced from draw. call method from draw? how
+    // will that work?
+	var scoreDetails = function(mm) {
+		  var steps = [], 
+		  nm = function(){syns[mm].note.seq(varInSeqPar, beetsVar);}, 
+		  sp = function(){syns[mm].note.seq(Harmony.notesReturn(-12, 4, 8), beetsVar);}, 
+		  pm = function(){syns[mm].note.seq(Harmony.notesReturn(0, 4, 8), Harmony.beetsReturn(1));}, 
+		  sto = function(){syns[mm].note.seq.stop();},
+		  functions = [nm, sp, pm, sto];
+		 // if (rain){
+		    varInSeqPar = Harmony.notesReturn(0,16,17);
+		    beetsVar = Harmony.beetsReturn(2);
+		//  }
+		  // else if (mm = musicMakers[2]) {
+		  //   varInSeqPar = ReturnBassNotesArray(0,1,2, 2);
+		  //   beetsVar = [4,4,4,4,2,2,2,2];
+		  // }
+		  // else {
+		  //   varInSeqPar = Harmony.notesReturntesArray();
+		  // }
+		  //going to need object score will be referencing
+		  for (var i = 0; i < scorePhrases; i++){
+		    if (i == 0){
+		      steps.push(0);
+		     // console.log(steps[i] + i);
+		    }
+		    //prevent seq from repeating itself (but this is not accounting for all circumstances, must solve )
+		    else if (i == 1 || steps[i-2] == sto) {
+		      var n =  functions[floor(random(functions.length - 1))] ;
+		      steps.push(n)
+		    }
+		    else if ((i+2)%2==0 ) {
+		      //length of each step
+		      steps.push(measures(vanillaMeasures[floor(random(vanillaMeasures.length))]));
+		    //  console.log(steps[i] + i);
+		    }
+		    
+		    else {
+		      var n =  functions[floor(random(functions.length))] ;
+		      steps.push(n);
+		    //  console.log(n + i + mm);
+		    }
+		  }
 
-// function notesArray(oct, lowRange, highRange) {
+		    return steps;
+		};
 
-// }
+	return {
+		publicSyns: syns,
+		//need a for loop to create each instrument and add to the syn array
+		// groupsynths is create a whole group of individual synthcreates
+		groupSysnths: function(q) {
+			// q is number of instruments to create
 
-// function chordArray(oct, lowRange, highRange) {
-//     var scoreChord = [];
-//     for (var i = 0; i < floor(random(lowRange,highRange)); i++) {
-//       scoreChord[i] = vanillaNotes[floor(random(0,vanillaNotes.length))] + oct;
-//     }
-//     return scoreChord;
-// }
+		for (var i = 0; i <= q; i++){
+			songG = new Song.synthCreate (i, 'lead', 'oo')
+			songG.make()
+			//Song.scoreCreate(i);
+		}
+		Song.scoreCreate(0);
+		Song.scoreCreate(1);
+		Song.scoreCreate(2);
+		},
+		synthCreate:	  function (name, kind, pre) {
+		  var leadInstruments = [FM, Pluck, Synth], padInstruments = [Synth2];
+		  // name - name object, kind - role of instrument (lead, pad etc), pre- preset,
+		  //reference item by spot in syns array... current plan is to assign each to specific
+		  // spot. syns[0] = lead, 1 = pad, 2 = bass, 4 = lead2, 5 = noise, 6 = drums
+		  this.name = name;
+		   
+		  this.make = function() {
+		   if (kind == 'lead') {
+		    var instrumentKind = leadInstruments[0];
+		    // if (instrumentKind == Pluck){
+		    //     pre = {};
+		    //   }
+		    // else if (instrumentKind == Synth){
+		    //     pre = presets[floor(random(3))];
+		    //   }
+		    // else if (instrumentKind != Synth || Pluck){
+		    pre = 'glockenspiel';
+		      //}
+		        //create the synth object 
+		    console.log( this.name + " is born " + pre + ' pre ' )
+		    name = instrumentKind(pre)
+		    if (instrumentKind == Pluck) {
+		      name.amp(2.25)
+		    }
+		    if (!syns[0]){
+		      syns.push(name);
+		  }
+		    else {
+		      syns[0] = name;
+		    }
+		  }
+
+		  // else if (kind == 'pad') {
+		  //   var instrumentKind = padInstruments[0],
+		  //   pre = padPresets[floor(random(padPresets.length))];
+		  //     //create the synth object 
+		  //   console.log( this.name + " is born " + pre + ' pre ' )
+		  //   name = instrumentKind(pre)
+		  //   if (!syns[1]){
+		  //     syns.push(name);
+		  // }
+		  //   else {
+		  //     syns[1] = name;
+		  //   }
+		  // }
+
+		  // else if (kind == 'bass'){
+		  //   console.log( this.name + " is born " + pre + ' pre ')
+		  //   name = Mono('waveBass')
+		  //   syns.push(name);
+		  //   }
+		  }
+		},
+		clear: function(c) {
+			syns[c].kill();
+
+		}, 
+		scoreCreate: function(q) {
+			var ss = scoreDetails(q);
+  			score = Score(ss).start().loop()
+			//s = Score([scoreDetails(syns[q])]).start().loop();
+			console.log(q + " . q ")
+		}
+	} ;
+
+})();
